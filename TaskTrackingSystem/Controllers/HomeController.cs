c3491beb71bd4c668e63d6fbedfd86b776ecfe01
Gihan Akalanka <gihanakalanka04@gmail.com>
2020-04-15 10:34:16 +0530

    add apsec

--- TaskTrackingSystem/Controllers/HomeController.cs
+++ TaskTrackingSystem/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 ﻿using Authentication;
+using SLA_Authentication_DLL;
+using System;
+using System.Data;
 using System.Web.Mvc;
 
 namespace TaskTrackingSystem.Controllers
@@ -6,6 +9,35 @@ namespace TaskTrackingSystem.Controllers
     public class HomeController : Controller
     {
         public ActionResult Index()
+        {
+            ViewBag.Title = "Welcome to Task Tracking System";
+
+            var currentUserServer = (Request.ServerVariables["LOGON_USER"]) != "" ? Request.ServerVariables["LOGON_USER"] : System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            var loggedUser = currentUserServer.Substring(currentUserServer.LastIndexOf("\\", StringComparison.Ordinal) + 1).ToUpper();
+
+            if (loggedUser != "")
+            {
+                var role = new clsRole();
+                DataTable dt = role.getUserRolesforApplication(loggedUser, "TTS");
+
+                if (dt != null && dt.Rows.Count > 0)
+                {
+                    var Role = dt.Rows[0][1].ToString();
+                }
+                else
+                {
+                    return RedirectToAction("Unauthorized", "Home");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Unauthorized", "Home");
+            }
+
+            return View();
+        }
+
+        public ActionResult Unauthorized()
         {
             return View();
         }
